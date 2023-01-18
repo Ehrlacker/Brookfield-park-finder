4c95633bf03a996f0c139a832bc5045bb4334559
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2023-01-17 21:24:30 -0500

    Change HomePageDescription flex direction

--- Client/src/Pages/HomePage/HomePage.jsx
+++ Client/src/Pages/HomePage/HomePage.jsx
@@ -13,16 +13,18 @@ const HomePage = () => {
       <Nav />
       <MainPageBackground />
       <HomePageDescription />
-      <HomePageDirections
-        image={Canyon}
-        title={"Find Your Park"}
-        description="Type in the letters of your selected state and find all of the national parks within that location."
-      />
-      <HomePageDirections
-        image={Trail}
-        title={"Learn More"}
-        description="Simply click on the details button to find park information such as the par activities, location, weather and much more. "
-      />
+      <div className="HomePageDirectionsContainer w-screen flex justify-around">
+        <HomePageDirections
+          image={Canyon}
+          title={"Find Your Park"}
+          description="Type in the letters of your selected state and find all of the national parks within that location."
+        />
+        <HomePageDirections
+          image={Trail}
+          title={"Learn More"}
+          description="Simply click on the details button to find park information such as the par activities, location, weather and much more. "
+        />
+      </div>
       <Footer />
     </div>
   )
--- Client/src/components/HomePageDirections/HomePageDirections.jsx
+++ Client/src/components/HomePageDirections/HomePageDirections.jsx
@@ -4,25 +4,25 @@ import "./HomePageDirections.css"
 
 const HomePageDirections = (props) => {
   return (
-    <div className="homePageDirections w-full z-3 mb-20">
-      <div className="ImgTextContainer flex justify-center items-center flex-col w-full">
-        <img
-          className="homeDirectionsImg  max-w-[30rem] max-h-[50rem]"
-          alt="A park"
-          src={props.image}
-        />
-        <div className="homePageDirectionsText md:w-full h-full text-white flex items-center justify-center flex-col z-1 p-8 max-w-[30rem] max-h-[20rem]">
-          <h1 className="directionsTitle text-center max-w-[30] max-h-[25] text-4xl">
-            {props.title}
-          </h1>
-          <p className="directionsParagrph text-3xl text-center max-w-[30] max-h-[25]">
-            {props.description}
-          </p>
-          <button className="home-directions-button text-xl mt-6 rounded-3xl border-solid border-2 border-white px-2">
-            <Link to="/Search">Start here</Link>
-          </button>
-        </div>
+    // <div className="homePageDirections w-full z-3 mb-20">
+    <div className="ImgTextContainer flex justify-center items-center flex-col mt-8">
+      <img
+        className="homeDirectionsImg  max-w-[30rem] max-h-[50rem]"
+        alt="A park"
+        src={props.image}
+      />
+      <div className="homePageDirectionsText md:w-full h-full text-white flex items-center justify-center flex-col z-1 p-8 max-w-[30rem] max-h-[20rem]">
+        <h1 className="directionsTitle text-center max-w-[30] max-h-[25] text-4xl">
+          {props.title}
+        </h1>
+        <p className="directionsParagrph text-3xl text-center max-w-[30] max-h-[25]">
+          {props.description}
+        </p>
+        <button className="home-directions-button text-xl mt-6 rounded-3xl border-solid border-2 border-white px-2">
+          <Link to="/Search">Start here</Link>
+        </button>
       </div>
+      {/* </div> */}
     </div>
   )
 }
