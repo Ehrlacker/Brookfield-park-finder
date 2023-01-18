a3d61895d8a07b5d823dbef39e44027e2cdeb61a
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2023-01-17 20:38:23 -0500

    Fix Vercel issues

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,14 +1,13 @@
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
-import Search from './pages/Search/Search'
-
+import Search from "./pages/Search/Search"
 
 const App = () => {
   return (
     <Routes>
-    <Route path="/" element={ <HomePage />} />
-     <Route path="/Search" element={ <Search />} />
-      </Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Search" element={<Search />} />
+    </Routes>
   )
 }
 export default App
